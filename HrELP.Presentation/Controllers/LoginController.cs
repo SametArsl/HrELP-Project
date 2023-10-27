03467de6c1980145bff534c8ac9ddc8c0bfa4087
Ömer Faruk ALMALI <omeralmali1@gmail.com>
2023-10-27 07:16:01 +0000

    Updated LoginController.cs

--- HrELP.Presentation/Controllers/LoginController.cs
+++ HrELP.Presentation/Controllers/LoginController.cs
@@ -103,5 +103,67 @@ namespace HrELP.Presentation.Controllers
 			}
 			return View(vm);
 		}
+
+         [Route("{Controller}/{Action}")]
+        public IActionResult ForgetPassword()
+        {
+            return View();
+        }
+
+        [Route("{Controller}/{Action}")]
+        [HttpPost]
+        public async Task<IActionResult> ForgetPassword(ForgetPasswordVM vm)
+        {
+            AppUser user = await _appUserService.GetUserWithEmailAsync(vm);
+
+            if (user != null && await _userManager.IsEmailConfirmedAsync(user))
+            {
+                var passwordToken = await _userManager.GeneratePasswordResetTokenAsync(user);
+                var resetLink = Url.Action("CreatePassword", "Login",
+                    new { email = user.Email, token = passwordToken }, Request.Scheme
+                );
+
+                string HtmlBody = "";
+                var PathToFile = Path.Combine(_IWebHostEnvironment.WebRootPath, "EmailTemplate", "index.html");
+
+                var builder = new BodyBuilder();
+                using (StreamReader sr = System.IO.File.OpenText(PathToFile))
+                {
+                    builder.HtmlBody = sr.ReadToEnd();
+                }
+
+                builder.HtmlBody = builder.HtmlBody.Replace("{0}", user.FullName);
+                builder.HtmlBody = builder.HtmlBody.Replace("{1}", resetLink);
+                builder.HtmlBody = builder.HtmlBody.Replace("{2}", resetLink);
+
+                MailMessage mail = new MailMessage();
+                mail.To.Add(user.Email);
+                mail.From = new MailAddress("noreplyhrelp@gmail.com");
+                mail.Subject = "Forget Password";
+                mail.Body = builder.HtmlBody;
+                mail.IsBodyHtml = true;
+
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    ServicePointManager.ServerCertificateValidationCallback = (s, certificate, chain, sslPolicyErrors) => true;
+                    smtp.EnableSsl = true; 
+                    smtp.UseDefaultCredentials = false;
+                    smtp.Credentials = new NetworkCredential("noreplyhrelp@gmail.com", "rmzqiazgoktnbcac");
+
+                    try
+                    {
+                        smtp.Send(mail);
+                        return RedirectToAction("Login", "Login");
+                    }
+                    catch (Exception ex)
+                    {                        
+                        Console.WriteLine("E-posta gönderme hatası: " + ex.Message);
+                    }
+                }               
+            }
+
+            return View();
+        }
+
 	}
 }
