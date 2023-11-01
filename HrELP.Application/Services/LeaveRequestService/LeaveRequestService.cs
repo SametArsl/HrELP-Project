287658923f61951d4a21cc23ed65002c5625b2f3
Ömer Faruk ALMALI <omeralmali1@gmail.com>
2023-11-01 00:29:49 +0000

    Updated LeaveRequestService.cs

--- HrELP.Application/Services/LeaveRequestService/LeaveRequestService.cs
+++ HrELP.Application/Services/LeaveRequestService/LeaveRequestService.cs
@@ -1,10 +1,11 @@
 ﻿using AutoMapper;
 using HrELP.Domain.Entities.Concrete.Requests;
+using HrELP.Domain.Entities.Enums;
 using HrELP.Domain.Repositories;
-using HrELP.Infrastructure.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,20 +20,42 @@ namespace HrELP.Application.Services.LeaveRequestService
             _leaveRequestRepository = leaveRequestRepository;
         }
 
-        public async Task CreateRequest(LeaveRequest request)
+        public async Task CreateLeaveRequestAsync(LeaveRequest request)
         {
             await _leaveRequestRepository.AddAsync(request);
         }
 
-        public List<LeaveRequest> GetAll()
-        {
-            return _leaveRequestRepository.GetAllWithAppUserAsync().ToList();
+        public async Task DeniedLeaveRequestAsync(int id)
+        {            
+            await _leaveRequestRepository.DeleteAsync(await _leaveRequestRepository.GetFirstOrDefaultAsync(x => x.Id == id));
         }
 
-        public async Task<LeaveRequest> GetRequestById(int id)
+        public async Task<LeaveRequest> GetLeaveRequestAsync(int leaveRequestId)
         {
-            LeaveRequest request = _leaveRequestRepository.GetById(id);
-            return request;
+           return  await _leaveRequestRepository.GetByIdAsync(leaveRequestId);
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestAsync()
+        {
+            return await _leaveRequestRepository.GetAllAsync(x=> x.IsActive==true);    
+        }
+
+        public async Task<List<LeaveRequest>> GetPermissionsByApprovalStatusAsync(ApprovalStatus approvalStatus, int id)
+        {
+            var permissions = await _leaveRequestRepository.GetAllAsync(x => x.IsActive == true);
+            return permissions.Where(p => p.ApprovalStatus == approvalStatus && p.UserId == id).ToList();
+        }
+
+        public async Task<List<LeaveRequest>> GetLeaveRequestByStatusAsync(ApprovalStatus approvalStatus, int id, params Expression<Func<LeaveRequest, object>>[] includes)
+        {
+          var leaveRequest=await _leaveRequestRepository.GetAllAsync(x=>x.IsActive==true,p=>p.LeaveType); 
+            return leaveRequest.Where(p=>p.ApprovalStatus==approvalStatus && p.UserId==id).ToList();            
+        }
+
+        public async  Task<List<LeaveRequest>> GetLeaveRequestByStatusAsync(int id, params Expression<Func<LeaveRequest, object>>[] includes)
+        {
+            var leaveRequest = await _leaveRequestRepository.GetAllAsync(x => x.IsActive == true, p => p.LeaveType);
+            return leaveRequest;
         }
     }
 }
