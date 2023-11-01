05c71b67eb3b857a83f499ee6ddb623b647bc8a8
Ömer Faruk ALMALI <omeralmali1@gmail.com>
2023-11-01 00:19:53 +0000

    Added ILeaveTypeRepository.cs

---
+++ HrELP.Domain/Repositories/ILeaveTypeRepository.cs
@@ -0,0 +1,13 @@
+using HrELP.Domain.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrELP.Domain.Repositories
+{
+    public interface ILeaveTypeRepository:IBaseRepository<LeaveType>
+    {
+    }
+}
